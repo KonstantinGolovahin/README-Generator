9c8c76deded6b03019506019de294270f7c2e3f0
unknown <sobakoved@inbox.lv>
2023-02-17 15:55:22 +0000

    Adds a list for selecting licences and set of icons for badges

--- index.js
+++ index.js
@@ -34,6 +34,14 @@ inquirer
         },
         // Licence should be a list of options based on a separate list and list of badges
         {
+            type: 'checkbox',
+            name: 'UserLicence',
+            message: 'Which licence to use?',
+            choices: [
+              'a', 'n', 'c', 'd',
+            ],
+          },
+        {
             type: 'input',
             message: 'Contributing',
             name: 'userContributing',
@@ -60,25 +68,36 @@ inquirer
         // console.log(response.username);
         // destructure response object   
         const {
-            username,
-            userlocation,
-            userURL
+            userTitle,
+            userDescription,
+            userInstallation,
+            userUsage,
+            userLicence,
+            userContributing,
+            userTests,
+            userGitHub,
+            userEmail,
+            
         } = response;
 
         // literals for building a page elements
-        const myHeader = `<h1>${username}</h1>`;
-        const myLocation = `<p  style="color:red;">${userlocation}</p>`;
-        const myURL = `<a href=${userURL}> my Page</a>`;
+       // const myHeader = `<h1>${username}</h1>`;
+       // const myLocation = `<p  style="color:red;">${userlocation}</p>`;
+       // const myURL = `<a href=${userURL}> my Page</a>`;
 
         
 
-        // append whole HTML to a file
-        fs.appendFile('index.html', stringHTML, (err) => {
-            if (err) throw err;
-        });
+       // // append whole HTML to a file
+       // fs.appendFile('index.html', stringHTML, (err) => {
+       //     if (err) throw err;
+       // });
     }
     );
 
+
+    // fro mhttps://gist.github.com/lukas-h/2a5d00690736b4c3a7ba -- Add tp my README!
+    const licenceMIT ="[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    const licenceGNUv3 = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
 
 
 
